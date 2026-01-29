c39d0f500eff30e05dd3a2d19f8b22fe8266be14
Oebe Rademaker <oeberademaker@desktop-kgkct78.home>
2026-01-29 20:31:35 +0100

    Added the cozy café project, reworked some heights to fit multiple devices. Also added new images, removed old ones, and restructured the project slightly.

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,10 +7,11 @@ const projects = defineCollection({
     schema: ({ image }) =>
         z.object({
             title: z.string(),
-            date: z.date(),
+            date: z.preprocess((val) => new Date(val as string), z.date()),
             tags: z.array(z.string()).optional(),
             thumbnail: image().optional(),
             excerpt: z.string().optional(),
+            order: z.number().optional(),
         }),
 });
 
